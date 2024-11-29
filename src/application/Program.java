8443573019d1a3337ae0b6284e5199158750580e
arthpr <arthurrosendo272@gmail.com>
2024-11-29 14:10:18 -0300

    Insert Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,10 @@
 package application;
 
-import java.util.List;
+import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -11,12 +12,15 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		//seller = sellerDao.findById(2);
+		Department department = new Department(3, null);
+		//List<Seller> seller = sellerDao.findAll();
+		//seller.forEach(System.out::println);
 		
-		//Seller seller = sellerDao.findById(2);
-		//Department department = new Department(3, null);
-		List<Seller> seller = sellerDao.findAll();
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
 		
-		seller.forEach(System.out::println);
+		System.out.println("Inserted! New id: " + newSeller.getId());
 	}
 
 }
