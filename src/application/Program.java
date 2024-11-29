b684204d66e03bb3afe5b2354c4dc487f16c4792
arthpr <arthurrosendo272@gmail.com>
2024-11-29 14:39:28 -0300

    Update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,15 +12,20 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		//seller = sellerDao.findById(2);
-		Department department = new Department(3, null);
+		
+		Seller seller = sellerDao.findById(1);
+		seller.setName("Neymar");
+		sellerDao.update(seller);
+		System.out.println(seller);
+		
+		//Department department = new Department(3, null);
+		
 		//List<Seller> seller = sellerDao.findAll();
 		//seller.forEach(System.out::println);
 		
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-		sellerDao.insert(newSeller);
-		
-		System.out.println("Inserted! New id: " + newSeller.getId());
+		//Seller newSeller = new Seller(null, "Jordan", "greg@gmail.com", new Date(), 4000.0, department);
+		///sellerDao.insert(newSeller);
+		//System.out.println("Inserted! New id: " + newSeller.getId());
 	}
 
 }
